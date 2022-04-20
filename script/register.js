63a33702b31b2101af47dd467bc884a0efbc3597
UshaGeorge <usha.dm@gmail.com>
2022-04-21 00:40:40 +0530

    Password strength validation
    Final Commit

--- script/register.js
+++ script/register.js
@@ -32,6 +32,7 @@ function validate(){
     var pwd= document.getElementById('txtPwd').value;
     var pwd2= document.getElementById('txtPwd2').value;
     var phoneNum = document.getElementById('txtPhone').value;
+   
 
     var emailRegExp =   /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var phoneRegExp = /^\d{3}[-. ]\d{3}[-. ]\d{4}$/;
@@ -89,3 +90,57 @@ function validate(){
     return isValid;
     //return false;
 }
+
+
+var strength = {
+    0: "Worst",
+    1: "Bad",
+    2: "Weak",
+    3: "Good",
+    4: "Strong"
+  }
+
+ 
+
+var password = document.getElementById('txtPwd');
+var meter = document.getElementById('password-strength-meter');
+var text = document.getElementById('password-strength-text');
+
+password.addEventListener('input', function() {
+  var val = password.value;
+  var result = zxcvbn(val);
+
+  // Update the password strength meter
+  meter.value = result.score;
+
+  // Update the text indicator
+  if (val !== "") {
+    text.innerHTML = "Strength: " + strength[result.score]; 
+    
+    switch(result.score){
+        case 1:
+            //meter.style.color='red';
+            console.log('red');
+            meter.classList.add("redMeter");
+            break;
+        case 2:
+            meter.style.color='yellow';
+            console.log('yellow');
+            break;
+        case 3:
+            meter.style.color='orange';
+            console.log('orange');
+            break;
+        case 4:
+            meter.style.color='green';
+            console.log('green');
+            break;
+        default:
+            meter.style.color='blue';
+            console.log('blue');
+            break;
+    }
+  } else {
+    text.innerHTML = "";
+  }
+});
